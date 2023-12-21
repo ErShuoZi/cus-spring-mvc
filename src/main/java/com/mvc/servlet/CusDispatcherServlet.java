94ef0953473b0e9ef88c61aa749ff77fc2e268d0
ershuozi <liushuo2951@163.com>
2023-12-21 09:26:52 +0800

    init cus-springmvc

---
+++ src/main/java/com/mvc/servlet/CusDispatcherServlet.java
@@ -0,0 +1,11 @@
+package com.mvc.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 1.CusDispatcherServlet充当原生DispatcherServlet
+ * 2.本质是Servlet，即继承HttpServlet
+ */
+public class CusDispatcherServlet  extends HttpServlet {
+
+}
