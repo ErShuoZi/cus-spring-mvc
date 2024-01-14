4855c5df99d8a797dcd3fca01c0b2d655691fc76
ershuozi <liushuo2951@163.com>
2024-01-14 19:44:40 +0800

    自定义SpringMvc 9.请求时,经过分发器(executeDispatch),分发器中会进行查找，成功则通过反射调用方法

--- src/main/java/com/controller/OrderController.java
+++ src/main/java/com/controller/OrderController.java
@@ -1,7 +1,26 @@
 package com.controller;
 
 import com.mvc.annotation.Controller;
+import com.mvc.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class OrderController {
+    @RequestMapping(value = "/list/order")
+    public void listOrders(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=utf-8");
+        //获取writter 返回信息
+        try {
+            PrintWriter writer = response.getWriter();
+            writer.print("<h1>Order List Info</h1>");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
--- src/main/java/com/mvc/servlet/CusDispatcherServlet.java
+++ src/main/java/com/mvc/servlet/CusDispatcherServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 
@@ -22,32 +24,36 @@ public class CusDispatcherServlet extends HttpServlet {
     //定义属性HandlerList,保存CusHandler(URL和控制器方法的映射)
     private List<CusHandler> HandlerList = new ArrayList<CusHandler>();
     private CusWebApplicationContext cusWebApplicationContext = null;
+
     @Override
     public void init() throws ServletException {
         //前端控制器DispatcherServlet是随着Tomcat的启动时启动,在前端控制器DispatcherServlet启动的时候初始化容器
         cusWebApplicationContext = new CusWebApplicationContext();
         cusWebApplicationContext.init();
+        //完成url和控制器方法的映射
         initHandlerMapping();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("CusDispatcherServlet---doGet方法被调用");
+        //调用方法完成请求分发
+        executeDispatch(req, resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("CusDispatcherServlet---doPost方法被调用");
+        doGet(req, resp);
     }
 
     //完成url和控制器方法的映射
     public void initHandlerMapping() {
         //当前ioc容器中是否为空？
-        if (cusWebApplicationContext.ioc.isEmpty()){
-          return;
-        }else {
+        if (cusWebApplicationContext.ioc.isEmpty()) {
+            return;
+        } else {
             //不为空.遍历ioc容器，然后进行url映射
-           //遍历map
+            //遍历map
             Set<Map.Entry<String, Object>> entries = cusWebApplicationContext.ioc.entrySet();
             for (Map.Entry<String, Object> entry : entries) {
                 //先取出注入的Object的Class对象
@@ -58,17 +64,55 @@ public class CusDispatcherServlet extends HttpServlet {
                     //遍历方法
                     for (Method declaredMethod : declaredMethods) {
                         //再去判断方法上是否存在RequestMapping注解
-                      if (declaredMethod.isAnnotationPresent(RequestMapping.class)) {
-                          //取出注解值
-                          RequestMapping declaredAnnotation = declaredMethod.getDeclaredAnnotation(RequestMapping.class);
-                          String url = declaredAnnotation.value();
-                          //创建一个CusHandler对象,该对象就是一个映射关系了
-                          CusHandler cusHandler = new CusHandler(url,entry.getValue(),declaredMethod);
-                          //将该对象(映射关系)放入到集合
-                          HandlerList.add(cusHandler);
-                      }
+                        if (declaredMethod.isAnnotationPresent(RequestMapping.class)) {
+                            //取出注解值
+                            RequestMapping declaredAnnotation = declaredMethod.getDeclaredAnnotation(RequestMapping.class);
+                            String url = declaredAnnotation.value();
+                            //创建一个CusHandler对象,该对象就是一个映射关系了
+                            CusHandler cusHandler = new CusHandler(url, entry.getValue(), declaredMethod);
+                            //将该对象(映射关系)放入到集合
+                            HandlerList.add(cusHandler);
+                        }
                     }
                 }
+            }
+        }
+    }
+
+    //通过请求request对象进行HandlerList查找,匹配则调用分发方法
+    private CusHandler getCusHandler(HttpServletRequest request, HttpServletResponse response) {
+        String requestURI = request.getRequestURI();
+        //遍历HandlerList
+        for (CusHandler cusHandler : HandlerList) {
+            if (cusHandler.getUrl().equals(requestURI)) {
+                //匹配上了
+                return cusHandler;
+            }
+        }
+        return null;
+    }
+
+
+    //完成分发
+    public void executeDispatch(HttpServletRequest request, HttpServletResponse response) {
+        CusHandler cusHandler = getCusHandler(request, response);
+        if (cusHandler != null) {
+            //匹配成功，反射调用控制器的方法
+            try {
+                cusHandler.getMethod().invoke(cusHandler.getController(), request, response);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            //说明请求的资源不存在,返回404
+            try {
+                response.setContentType("text/html;charset=utf-8");
+                PrintWriter writer = response.getWriter();
+                writer.print("<h1>404 NOT FOUND</h1>");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
